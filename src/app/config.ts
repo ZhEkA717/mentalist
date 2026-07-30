25a9c63ad3c231b654fd316cc96b72fec3fb219e
Zheka <grushevskiy.yevgeniy@mail.ru>
2026-07-30 18:14:42 +0300

    feat: read from .env

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,11 +1,4 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCvImyA0TtxxIrHDYHhLKnBqxY-Xd9_AiE",
-  authDomain: "mentalist-f4d9e.firebaseapp.com",
-  projectId: "mentalist-f4d9e",
-  storageBucket: "mentalist-f4d9e.firebasestorage.app",
-  messagingSenderId: "68210376129",
-  appId: "1:68210376129:web:36c6eab65d6d20a0f94340",
-  measurementId: "G-YQ6QN4JTJ7"
-};
+import { environment } from '../environments/environment';
 
-export const cloudflareWorkerUrl = 'https://falling-meadow-a840.grushevskiy-yevgeniy.workers.dev/';
+export const firebaseConfig = environment.firebaseConfig;
+export const cloudflareWorkerUrl = environment.cloudflareWorkerUrl;

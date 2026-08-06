8e6d94721896daf92811ddd68d4329a7166a2e5a
Zheka <grushevskiy.yevgeniy@mail.ru>
2026-08-06 10:40:09 +0300

    feat: hero adaptive 950

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('../modules/landing/pages/landing/landing.component').then(m => m.LandingComponent),
-    pathMatch: 'full'
+    loadChildren: () =>
+      import('../modules/landing/landing.routes').then(m => m.routes),
   }
 ];
--- src/modules/landing/landing.routes.ts
+++ src/modules/landing/landing.routes.ts
@@ -1,5 +1,5 @@
-import { Routes } from '@angular/router';
-import { LandingComponent } from './pages/landing/landing.component';
+import {Routes} from '@angular/router';
+import {LandingComponent} from './pages/landing/landing.component';
 
 export const routes: Routes = [
   {

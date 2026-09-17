247e8bb566ffd9d8cb8622bb581ac66fe6bb337e
Zheka <grushevskiy.yevgeniy@mail.ru>
2026-09-17 18:48:58 +0300

    feat: change language in calendar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { provideTaiga, tuiCommonIconsProvider } from '@taiga-ui/core';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, signal} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {provideHttpClient} from '@angular/common/http';
+import {provideTaiga, tuiCommonIconsProvider} from '@taiga-ui/core';
+import {TUI_LANGUAGE} from '@taiga-ui/i18n';
+import {TUI_RUSSIAN_LANGUAGE} from '@taiga-ui/i18n/languages/russian';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
-    provideTaiga({ mode: 'dark' }),
+    provideTaiga({mode: 'dark'}),
     tuiCommonIconsProvider({
       close: '',
       check: '',
     }),
+    {provide: TUI_LANGUAGE, useValue: signal(TUI_RUSSIAN_LANGUAGE)},
   ]
 };
